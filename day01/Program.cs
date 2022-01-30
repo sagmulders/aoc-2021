3e74d2ae8479f20991ab93fec82f255002e3b772
Serge Mulders <sergemulders@Serges-MacBook-Air.local>
2022-01-30 22:58:46 +0100

    solution day 1

--- day01/Program.cs
+++ day01/Program.cs
@@ -1,2 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+var path = Path.Combine(Environment.CurrentDirectory, "data");
+Console.WriteLine(path);
+
+var data = File.ReadAllLines(path);
+Console.WriteLine($"Numbre of lines: {data.Length}");
+
+var prev = 0;
+var increments = 0;
+
+foreach (var line in data)
+{
+    int value = int.Parse(line);
+
+    if (prev != 0)
+    {
+        if (value>prev) increments++;
+    }
+
+    prev = value;
+}
+
+Console.WriteLine($"Increments: {increments}");
