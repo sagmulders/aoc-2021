5558aa8aea97e949a571976ce76c8c6913cb24a9
Serge Mulders <sergemulders@Serges-MacBook-Air.local>
2022-02-05 21:07:09 +0100

    day 7 part 2

--- day07/Program.cs
+++ day07/Program.cs
@@ -1,13 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 var data = File.ReadAllText(Path.Combine(Environment.CurrentDirectory, "data"));
-var positions = data.Split(',').ToArray().Select(x=>int.Parse(x));
+var positions = data.Split(',').ToArray().Select(x => int.Parse(x));
 
 int[] fuels = new int[positions.Count()];
+var costs = Enumerable.Range(1, positions.Max()+1).ToArray();
 
 for (int i = 0; i < positions.Count(); i++)
 {
-    positions.ToList().ForEach(X=> fuels[i] += Math.Abs(X-(i+1)));
+    positions.ToList().ForEach(X =>
+    {
+        var diff = Math.Abs(X - (i + 1));
+        fuels[i] += costs.Take(diff).Sum();
+    });
 }
 
-Console.WriteLine(fuels.OrderBy(x=>x).First());
+Console.WriteLine(fuels.OrderBy(x => x).First());
