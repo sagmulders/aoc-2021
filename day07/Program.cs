dfa20fd5152d9c5dfbef4fad31c0fbd8ec08955b
Serge Mulders <sergemulders@Serges-MacBook-Air.local>
2022-02-05 10:37:30 +0100

    day 7 part 1

--- day07/Program.cs
+++ day07/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+var data = File.ReadAllText(Path.Combine(Environment.CurrentDirectory, "data"));
+var positions = data.Split(',').ToArray().Select(x=>int.Parse(x));
+
+int[] fuels = new int[positions.Count()];
+
+for (int i = 0; i < positions.Count(); i++)
+{
+    positions.ToList().ForEach(X=> fuels[i] += Math.Abs(X-(i+1)));
+}
+
+Console.WriteLine(fuels.OrderBy(x=>x).First());
