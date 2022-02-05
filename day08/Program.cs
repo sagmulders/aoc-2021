c9eecf114f62022b1409898b0b2cc8dde31e8fca
Serge Mulders <sergemulders@Serges-MacBook-Air.local>
2022-02-05 21:56:34 +0100

    day 8 part 1

--- day08/Program.cs
+++ day08/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+var lines = File.ReadAllLines(Path.Combine(Environment.CurrentDirectory, "data"));
+var outputs = lines.Select(x=> x.Split('|')[1].Split(' ')).SelectMany(x=> x);
+
+var answer = outputs.Where(x=> new int[] { 2,3,4,7}.Contains(x.Length) ).Count();
+
+Console.WriteLine(answer);
